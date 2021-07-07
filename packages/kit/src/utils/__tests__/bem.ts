3f27961ce40997d8383cab50eafce2f8e27715b2
Johan Bergström <johan.bergstrom@isotop.se>
2021-07-07 08:54:01 +0200

    wip

---
+++ packages/kit/src/utils/__tests__/bem.ts
@@ -0,0 +1,51 @@
+import { createModule } from "../bem";
+
+describe("bem", () => {
+  it("gets block", () => {
+    const module = createModule("block");
+    expect(module()).toStrictEqual({ block: "block" });
+  });
+  it("gets block and element", () => {
+    const module = createModule("block", "element");
+    const expected = {
+      block: "block",
+      element: "block__element",
+    };
+    expect(module()).toStrictEqual(expected);
+  });
+  it("gets block and camelCased element property", () => {
+    const module = createModule("block", "one-element");
+    const expected = {
+      block: "block",
+      oneElement: "block__one-element",
+    };
+    expect(module()).toStrictEqual(expected);
+  });
+  it("gets block and multiple elements", () => {
+    const module = createModule("block", ["element", "second-element"]);
+    const expected = {
+      block: "block",
+      element: "block__element",
+      secondElement: "block__second-element",
+    };
+    expect(module()).toStrictEqual(expected);
+  });
+  it("gets block and no boolean modifier", () => {
+    const module = createModule(["block", "test"]);
+    const expected = { block: "block" };
+    expect(module()).toStrictEqual(expected);
+  });
+  it("gets block and boolean modifier", () => {
+    const module = createModule<{ test: boolean }>(["block", "test"]);
+    const expected = { block: "block block--test" };
+    expect(module({ test: true })).toStrictEqual(expected);
+  });
+  it("gets block and multiple choice modifier classes", () => {
+    const module = createModule<{ test: "one" | "two" | "three" }>([
+      "block",
+      ["test"],
+    ]);
+    const expected = { block: "block block--test-two" };
+    expect(module({ test: "two" })).toStrictEqual(expected);
+  });
+});
--- packages/kit/src/utils/bem.ts
+++ packages/kit/src/utils/bem.ts
@@ -1,10 +1,106 @@
 import clsx from "clsx";
 
-export const modifier = (blockOrElement: string, modifier: string) =>
+export const modifier = (blockOrElement: BemElement, modifier: string) =>
   `${blockOrElement}--${modifier}`;
 
-export const element = (block: string, element: string): string =>
+export const element = (block: BemElement, element: BemElement): string =>
   `${block}__${element}`;
+
+const kebabToCamel = (str: string): string =>
+  str
+    .split("-")
+    .map((item, index) =>
+      index
+        ? item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()
+        : item.toLowerCase()
+    )
+    .join("");
+
+type BemCssClass = string;
+type BemModifierParameter = string;
+type BemModifierValue = string;
+type BemModifier =
+  | BemModifierParameter
+  | [BemModifierParameter, BemModifierValue[]];
+type BemElementWithModifier =
+  | [BemCssClass, BemModifier]
+  | [BemCssClass, BemModifier[]];
+type BemElement = BemCssClass | BemElementWithModifier;
+
+export const elementWithModifiers = <Type>(
+  [element, modifiers]: BemElementWithModifier,
+  props?: Type
+): string => {
+  if (typeof modifiers === "string") {
+    const modifierClass = modifier(element, modifiers);
+    return clsx(element, { [modifierClass]: props[modifiers] });
+  }
+  return clsx(
+    element,
+    modifiers.reduce((prev, curr) => {
+      if (Array.isArray(curr)) {
+        const [modifierProp, values] = curr;
+        return clsx(
+          values.reduce((p, c) => {
+            const modifierClass = modifier(element, `${modifierProp}-${c}`);
+            return [...p, clsx({ [modifierClass]: props[modifierProp] === c })];
+          }, [])
+        );
+      }
+    }, [])
+  );
+};
+
+export const createModule = <Type extends Record<string, string | boolean>>(
+  block: BemElement,
+  elements?: BemElement | BemElement[]
+) => (props?: Type) => {
+  if (typeof elements === "undefined") {
+    return {
+      block: Array.isArray(block)
+        ? elementWithModifiers<Type>(block, props || ({} as Type))
+        : block,
+    };
+  }
+  if (typeof elements === "string") {
+    return {
+      block: Array.isArray(block)
+        ? elementWithModifiers<Type>(block, props)
+        : block,
+      [elements.includes("-") ? kebabToCamel(elements) : elements]: element(
+        block,
+        elements
+      ),
+    };
+  }
+  if (Array.isArray(elements)) {
+    const bemElements = elements.reduce(
+      (
+        previous: Record<string, string>,
+        current: BemElement | [string, BemModifier] | [string, BemModifier[]]
+      ) => {
+        if (typeof current === "string") {
+          return {
+            ...previous,
+            [current.includes("-") ? kebabToCamel(current) : current]: element(
+              block,
+              current
+            ),
+          };
+        }
+
+        return previous;
+      },
+      {}
+    );
+    return {
+      block: Array.isArray(block)
+        ? elementWithModifiers<Type>(block, props)
+        : block,
+      ...bemElements,
+    };
+  }
+};
 
 // function createModule([block, blockModifiers]: [string, ], [element, elementModifiers][]) {
 //   return {}
@@ -48,35 +144,3 @@ export const element = (block: string, element: string): string =>
 //     "heading",
 //   ]
 // );
-
-type BemCssClass = string;
-type BemModifierParameter = string;
-type BemModifierValue = string;
-type BemModifier =
-  | [BemModifierParameter, BemModifierValue]
-  | [BemModifierParameter, BemModifierValue[]];
-type BemElementWithModifier =
-  | [BemCssClass, BemModifier]
-  | [BemCssClass, BemModifier[]];
-type BemElement = BemCssClass | BemElementWithModifier;
-
-const elementWithModifiers = <Type>(
-  elementWithModifiers: BemElementWithModifier,
-  props: Type
-): Record<BemCssClass, boolean> => {
-  if (Array.isArray(modifiers)) {
-  }
-};
-
-const createModule = <Type>(
-  block: BemElement,
-  elements: BemElement | BemElement[]
-) => (props: Type) => {
-  if (typeof elements === "undefined") {
-    return {
-      block: Array.isArray(block)
-        ? elementWithModifiers<Type>(block, props)
-        : block,
-    };
-  }
-};
